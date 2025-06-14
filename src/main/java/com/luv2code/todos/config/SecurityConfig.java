caa5255936d224110b078f0489304625aea857d1
Mahmoud Gaber <71694567+MahmooudGaaber@users.noreply.github.com>
2025-06-14 05:30:35 +0300

    TODO : Setup Security Configuration

---
+++ src/main/java/com/luv2code/todos/config/SecurityConfig.java
@@ -0,0 +1,89 @@
+package com.luv2code.todos.config;
+
+import com.luv2code.todos.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class SecurityConfig {
+
+
+    private  final UserRepository userRepository ;
+    private  final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(UserRepository userRepository, JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.userRepository = userRepository;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(()->new UsernameNotFoundException("User Not Found"));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint(){
+        return ((request, response, authException) -> {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+            response.setHeader("WWW-Authentication","");
+            response.getWriter().write("{\"Error\":\"Unauthorized access\"}");
+        });
+    }
+
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception {
+        http.authorizeHttpRequests(
+                configuer-> configuer
+                        .requestMatchers("/docs/**","/swagger-ui/**","/v3/api-docs/**","/swagger-resources/**","/webjars/**").permitAll());
+                http.csrf(csrf -> csrf.disable());
+        http.exceptionHandling(
+                exceptionHandlingError -> exceptionHandlingError
+                        .authenticationEntryPoint(authenticationEntryPoint()));
+        http.sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+
+}
+
+
+
+
+
+
